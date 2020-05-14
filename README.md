bda00ba36493665998414e2545d0bd13b52fc329
David Garcia <david.garcia@adevinta.com>
2020-05-14 20:38:45 +0200

    adds initial congif files

---
+++ README.md
@@ -0,0 +1,2 @@
+# New Relic Apdex Board code challenge
+A project build with JavaScript and ReactJS.
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {render} from 'react-dom'
+import {HomePage} from './components/home'
+import './index.scss'
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={HomePage} />
+    </Router>
+  )
+}
+
+const DOMelement = document.querySelector('#app')
+
+render(<App />, DOMelement)
+
+export {App}
